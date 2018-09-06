64d1386e6b0963066ef063f6dec189928384d406
Nick Burns <nburns@users.noreply.github.com>
2018-09-05 22:00:33 -0700

    asset path problems

--- config.rb
+++ config.rb
@@ -44,10 +44,10 @@ page '/*.txt', layout: false
 # Build-specific configuration
 # https://middlemanapp.com/advanced/configuration/#environment-specific-settings
 
- configure :build do
-   activate :minify_css
-   activate :minify_javascript
- end
+configure :build do
+  activate :minify_css
+  activate :minify_javascript
+end
 
- set :build_dir, 'docs'
-
+set :build_dir, 'docs'
+set :relative_links, true
